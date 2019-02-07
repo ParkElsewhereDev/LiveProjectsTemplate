2e15a7bd54977ea326887bf64c344ec1a9638a0d
Park Elsewhere <Mehdi@Digitals-Mac-Pro.local>
2019-02-07 12:10:28 +0000

    Changed Credentials

--- docs/scripts/credentials.js
+++ docs/scripts/credentials.js
@@ -1,4 +1,4 @@
-window.urbanwildcredentials={
+window.parkelsewherecredentials={
   restlet: {
     "user": "2f907b28-1370-4f0d-bcb5-c9d18195034a",
     "pass": "62c01abc-a8a6-4b06-9522-aec047a51620"
